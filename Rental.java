fa934b8a3bd40e15c75880533da0b89d2f25fb00
devChanChan <withksc@gmail.com>
2020-02-03 14:24:57 -0500

    Extract Frequent Renter Points

--- Rental.java
+++ Rental.java
@@ -36,4 +36,11 @@ class Rental {
 
         return result;
     }
+
+    int getFrequentRenterPoints() {
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            return 2;
+        else 
+            return 1;
+    }
 }
